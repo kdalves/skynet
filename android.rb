5b0a9a7c87cde736fc614224eefea94f0a8a8bdb
Kathleen <kathleendeveloper@hotmail.com>
2019-06-11 00:02:53 -0300

    Added new validations
    Modified 'resetar' and 'biblioteca_de_registros' and to check if exist the number and added comment just to explain my logic

--- android.rb
+++ android.rb
@@ -1,23 +1,31 @@
 class Android
-	def numero	
+  def numero	# Gera novo numero de serie
     letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Z', 'W', 'Y', 'Z']
     gerador_letras = letras[rand(letras.length)]
     gerador_numeros = rand(0..9)
-    numero_de_serie = ''
-    
+    numero_de_serie = ''    
+	
     2.times{ numero_de_serie << gerador_letras }
     3.times{ numero_de_serie << gerador_numeros.to_s }
-    
+	
     return numero_de_serie
-	end
-  
-  def resetar(numero_que_ja_existe)
-    biblioteca_de_registros(numero_que_ja_existe)
-    numero
   end
   
-  def biblioteca_de_registros(numero_que_ja_existe)    
-    biblioteca='' if !biblioteca
+  def resetar(numero_anterior) # Reseta e avalia se o novo numero e igual ao anterior
+    novo_numero = numero
+    biblioteca = biblioteca_de_registros(numero_anterior)
+	
+    biblioteca.each do |b|
+      if (b == novo_numero)
+        resetar(numero_anterior)
+      end
+    end    
+	
+    return novo_numero
+  end
+  
+  def biblioteca_de_registros(numero_que_ja_existe)  # Faz uma biblioteca do que ja existe  
+    biblioteca=[] if !biblioteca
     biblioteca << numero_que_ja_existe
   end
 end
